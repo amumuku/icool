adb5eb0dacbdd35bae0f3d24af7b091b9ac18fcd
“suzhenyun” <shsuzhenyun@jd.com>
2018-11-19 19:35:40 +0800

    1 增加配置文件

--- freight/freight-server/src/main/java/com/general/freight/server/impl/BrandFacadeImpl.java
+++ freight/freight-server/src/main/java/com/general/freight/server/impl/BrandFacadeImpl.java
@@ -1,8 +1,14 @@
 package com.general.freight.server.impl;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.general.freight.client.BrandFacade;
+import com.tqmall.dolphin.dubbo.CustomerDubboService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Component;
 
 /**
  * @classname BrandFacadeImpl
@@ -14,6 +20,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service(version ="1.0.0.szy")
 public class BrandFacadeImpl implements BrandFacade {
+
+    @Autowired
+    private CustomerDubboService customerDubboService;
 
     @Override
     public String getSayHelloWorld() {
