459d00b92a3186945c47bd0d999419ec5ec317c0
“suzhenyun” <shsuzhenyun@jd.com>
2018-11-19 19:39:05 +0800

    1 增加配置文件

--- freight/freight-server/src/main/java/com/general/freight/server/Application.java
+++ freight/freight-server/src/main/java/com/general/freight/server/Application.java
@@ -4,12 +4,16 @@ package  com.general.freight.server;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * @author szy
  */
 @MapperScan("com.general.freight.dal.dao")
 @SpringBootApplication
+@ComponentScan("com.general.freight")
+@ImportResource(locations = "classpath:dubbo-consume.xml")
 public class Application {
 
 
